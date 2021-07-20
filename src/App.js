1de2e0ee33ccd845d4e3d0bdd4dad25cdb0e4037
Hamza <hamza.mebarek1@gmail.com>
2021-07-20 12:42:20 +0200

    updated app

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ const App = () => {
 
   return (
     <div>
-      <h1 className="title">Welcome Hamza</h1>
+      <h1 className="title">Welcome Oscar</h1>
 
       <SearchForm
         searchTerm={searchTerm}
