a48d601cbca4e1667d30f02b7d7d85e64c7f6af7
Hamza Mebarek <hamza.mebarek1@gmail.com>
2021-07-28 10:45:11 +0200

    Updated front with 0.9.0

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ const App = () => {
 
   return (
     <div>
-      <h1 className="title">Welcome to the last version 0.8.0</h1>
+      <h1 className="title">Welcome to the last version 0.9.0</h1>
 
       <SearchForm
         searchTerm={searchTerm}
